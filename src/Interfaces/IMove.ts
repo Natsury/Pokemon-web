33e8f45fe9893d8a3b59c36060d07977fb5ffbf1
Natsury <priez.lucas80@gmail.com>
2024-02-05 19:18:22 +0100

    Ajout de l'interface de Move

---
+++ src/Interfaces/IMove.ts
@@ -0,0 +1,7 @@
+export interface Move{
+    nom: string         //  Le nom du Move
+    type: string        //  Le type du Move
+    power: number       //  Puissance
+    accuracy: number    //  Précision
+    pp: number          //  Nombre d'utilisation du move
+}
