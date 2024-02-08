bb039e7bf16b57fa24e2b260c84ab3bbfcba4bd1
Natsury <priez.lucas80@gmail.com>
2024-02-08 16:53:43 +0100

    Ajout du fichier toolBox contenant les fonctions réccurentes comme les appels API

---
+++ src/Tools/toolBox.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**
+ * Effectue une requete HTTP à l'url demandée
+ * @param url l'url de la requete
+ * @returns Un json des infos demandées
+ */
+export async function getAPI(url:string) {
+    try{
+        let request = new Request(url)
+        let reponse = await fetch(request)
+        let json = await reponse.json()
+        return json
+    }catch(e){
+        console.log(e)
+        
+    }
+}
