2ee4d984c2bd297c15d5884cac8caf88aef5838d
Natsury <priez.lucas80@gmail.com>
2024-02-08 23:27:34 +0100

    Implémentation de la sélection des 4 compétences du pokémon

--- src/Interfaces/IMove.ts
+++ src/Interfaces/IMove.ts
@@ -1,6 +1,8 @@
+import { type } from "./IType"
+
 export interface Move{
     nom: string         //  Le nom du Move
-    type: string        //  Le type du Move
+    type: type        //  Le type du Move
     power: number       //  Puissance
     accuracy: number    //  Précision
     pp: number          //  Nombre d'utilisation du move
--- src/Pokemon/moves.tsx
+++ src/Pokemon/moves.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
 import { Move } from '../Interfaces/IMove';
+import { type } from '../Interfaces/IType';
+import * as typesDispo from "./types.tsx";
+import { getAPI } from "../Tools/toolBox.tsx"
+import { info } from 'console';
+
 
 /**
  * Récupère un tableau de moves et en sélectionne 4
@@ -7,11 +11,35 @@ import { Move } from '../Interfaces/IMove';
  * @returns un tableau de 4 moves 
  */
 export async function Moves(json){
-    let moves:Move[] = []
+    let moves:Move[] = []           //  Tableau des attaques du pokemon
+    let isPickedUp:Number[] = []    // Tableau des attaques déjà prises
+    
+    
+    while(moves.length != 4){
 
-    /*while(moves.length != 4){
+        let rand = Math.floor(Math.random() * json.length)
 
-    }*/
+        if(!isPickedUp.includes(rand)){
+
+            isPickedUp.push(rand)                                       //  On met l'indice dans le tableau des déjà pris
+
+            let infoMove = await getAPI(json[rand]["move"]["url"])             //  On récupère toutes les info du Move
+            let typeMove:type = getType(infoMove["type"]["name"])
+
+            let unMove:Move = {nom: json[rand]["move"]["name"], accuracy: infoMove["accuracy"], power: infoMove["power"],
+                                pp: infoMove["pp"], type: typeMove, category: infoMove["damage_class"]["name"]}
+
+            moves.push(unMove)
+        }
+    }
     return moves
 }
 
+/**
+ * Retourne le type demandé
+ * @param typeMove Type à récupérer
+ * @returns Un objet type || Retourne le type normal si le type demandé n'existe pas
+ */
+function getType(typeMove:string):type{
+    return typesDispo[typeMove] || typesDispo.normal
+}
