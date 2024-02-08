497619a31c300cee93548ce12fca1347ad7c6999
Natsury <priez.lucas80@gmail.com>
2024-02-08 22:17:01 +0100

    Ajout des types (faiblesses et immunités pas encore remplies)

---
+++ src/Pokemon/types.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { type } from "../Interfaces/IType";
+
+
+export const normal:type = {
+    nom: "normal",
+    color: "#FFFFFF",
+    weakness: [],
+    imunities: [],
+}
+
+export const fire:type = {
+    nom: "fire",
+    color: "#FFFFFF",
+    weakness: [],
+    imunities: [],
+}
+
+export const water:type = {
+    nom: "water",
+    color: "#4e90d6",
+    weakness: [],
+    imunities: [],
+}
+
+export const electrik:type = {
+    nom: "electrik",
+    color: "#f4d13b",
+    weakness: [],
+    imunities: [],
+}
+
+export const grass:type = {
+    nom: "grass",
+    color: "#60b953",
+    weakness: [],
+    imunities: [],
+}
+
+export const ice:type = {
+    nom: "ice",
+    color: "#73cec0",
+    weakness: [],
+    imunities: [],
+}
+
+export const fighting:type = {
+    nom: "fighting",
+    color: "#cd3f6a",
+    weakness: [],
+    imunities: [],
+}
+
+export const poison:type = {
+    nom: "poison",
+    color: "#a566c7",
+    weakness: [],
+    imunities: [],
+}
+
+export const ground:type = {
+    nom: "ground",
+    color: "#d97745",
+    weakness: [],
+    imunities: [],
+}
+
+export const flying:type = {
+    nom: "flying",
+    color: "#a7c1ef",
+    weakness: [],
+    imunities: [],
+}
+
+export const psychic:type = {
+    nom: "psychic",
+    color: "#f86d74",
+    weakness: [],
+    imunities: [],
+}
+
+export const bug:type = {
+    nom: "bug",
+    color: "#90c12d",
+    weakness: [],
+    imunities: [],
+}
+
+export const rock:type = {
+    nom: "rock",
+    color: "#c7b78b",
+    weakness: [],
+    imunities: [],
+}
+
+export const ghost:type = {
+    nom: "ghost",
+    color: "#5369ac",
+    weakness: [],
+    imunities: [],
+}
+
+export const dragon:type = {
+    nom: "dragon",
+    color: "#0c68bd",
+    weakness: [],
+    imunities: [],
+}
