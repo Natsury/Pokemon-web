67e7da260e3fcb03dc2e68c0cd70ba2a94e4884c
Natsury <priez.lucas80@gmail.com>
2024-02-16 11:54:53 +0100

    Fix warning + optimisation code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import CadreBattle from './CadreBattle';
+import CadreBattle from './CadreBattle.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
